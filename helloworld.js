7a72b8fe4f69a96d94b305ae28f002962af2fb6f
eric-filishtiner <eric.filishtiner@gmail.com>
2023-09-07 15:11:02 -0700

    Added new routes, static file server, fixed string literal processing bug

--- helloworld.js
+++ helloworld.js
@@ -1,10 +1,36 @@
 const http = require('http')
+const fs = require('fs')
 const port = process.env.PORT || 3000
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' })
-    res.end('Hello World!')
-})
+function serveStaticFile(res, path, contentType, responseCode = 200) {
+    fs.readFile(__dirname + path, (err, data) => {
+        if(err) {
+            res.writeHead(500, { 'Content-Type': 'text/plain' })
+            return res.end('500 - Internal Error')
+        }
+        res.writeHead(responseCode, {responseCode, { 'Content-Type': contentType}})
+        res.end(data)
+    })
 
-server.listen(port, () => console.log('server started on port ${port}; ' +
+}
+const server = http.createServer((req, res) => {
+    //normalize URL by removing querystring, optional
+    //trailing slash, and making it lowercase
+    const path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase()
+    switch(path) {
+        case '':
+            serveStaticFile(res, '/public/home.html', 'text/html')
+            break
+        case '/about':
+            serveStaticFile(res, '/public/about.html', 'text/html')
+            break
+        case '/img/logo.png':
+            serveStaticFile(res, '/public/img/logo.png', 'image/png')
+            break
+        default:
+            serveStaticFile(res, '/public/404.html', 'text/html', 404)
+            break
+    } })
+
+server.listen(port, () => console.log(`server started on port ${port}; ` +
     'press Ctrl-C to terminate....'))
